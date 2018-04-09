863f968bd6aa75f9b977c6d000dac14b99aab048
hakjoon <cho224621@gmail.com>
2018-04-09 15:24:16 +1000

    Cs code upload

---
+++ BattleShipsCs/src/Model/ShipName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame
+{
+    /// <summary>
+    /// The names of all of the ships in the gam
+    /// </summary>
+    /// <remarks></remarks>
+    public enum ShipName
+    {
+        None = 0,
+        Tug = 1,
+        Submarine = 2,
+        Destroyer = 3,
+        Battleship = 4,
+        AircraftCarrier = 5
+    }
+}
