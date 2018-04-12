81c83da0c5f7cbb013ca9ef8cd2fa0e6b1ce60a9
101866520 <101866520@student.swin.edu.au>
2018-04-12 13:07:55 +1000

    bug fix on incorrect coordinates

--- BattleShipsCs/DeploymentControl.cs
+++ BattleShipsCs/DeploymentControl.cs
@@ -116,10 +116,10 @@ namespace MyGame
             int row = 0;
             int col = 0;
             row = System.Convert.ToInt32(Math.Floor(
-                (mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)
+                (mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)
                 ));
             col = System.Convert.ToInt32(Math.Floor(
-                System.Convert.ToDouble(mouse.X - UtilityFunctions.FIELD_LEFT) / System.Convert.ToDouble(UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)
+                (mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)
                 ));
 
             if (row >= 0 & row < GameController.HumanPlayer.PlayerGrid.Height)
